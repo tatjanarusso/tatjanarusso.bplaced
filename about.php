9b4d9726d1de6c55a4836fa3a20ef0475d048789
Tatjana <tatjana.russo@edu.tbz.ch>
2022-03-10 14:03:07 +0100

    added new files

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php include 'include\header.php'; ?>
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                </div>
+            </div>
+        </section>
+    <?php include 'include\footer.php'; ?>
+</body>
+</html>
